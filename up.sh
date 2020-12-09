0dae7e662c83915bf9cec9fccc250b12ac9e58e6
Dafu Lv <dafu@a011926.armchina.com>
2020-12-09 19:12:30 +0800

    update up.sh with the correct git pull option
    Signed-off-by: Dafu Lv <dafu@a011926.armchina.com>

--- up.sh
+++ up.sh
@@ -2,22 +2,11 @@
 
 TOP=~/Lab/hafnium
 
-SERIAL="`date +%s`"
+pushd $TOP
 
-pushd $TOP >& /dev/null
-
-find . -name ".git" | grep "out" -vw | sort > /tmp/hafnium-$SERIAL
-
-while IFS= read -r line
-do
-    folder="`echo $line | sed 's/\/\.git//'`"
-    echo "checking $folder ..."
-    pushd $folder >& /dev/null
-    git pull
-    popd >& /dev/null
-done < "/tmp/hafnium-$SERIAL"
-
-rm -f /tmp/hafnium-$SERIAL
+echo "updating code ..."
+git pull
+git pull --recurse-submodules
 
 echo "updating index ..."
 cscope -Rkb
