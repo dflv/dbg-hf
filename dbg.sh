a5846bf5754c03d30a61265b3e32d78e31877915
Dafu Lv <dafu.lv@gmail.com>
2021-02-03 15:55:47 +0800

    update qemu to the local-built binary
    Signed-off-by: Dafu Lv <dafu.lv@gmail.com>

--- dbg.sh
+++ dbg.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-cd ..
-qemu-system-aarch64 -M virt,gic_version=3 -cpu cortex-a57 -smp 4 -nographic -machine virtualization=true -kernel out/reference/qemu_aarch64_clang/hafnium.bin -initrd ./dbg/initrd.img -append "rdinit=/sbin/init" -s -S
-cd - >& /dev/null
+pushd .. >& /dev/null
+
+../qemu-4.1.0/aarch64-softmmu/qemu-system-aarch64 \
+    -M virt,gic_version=3 \
+    -cpu cortex-a57 \
+    -smp 4 \
+    -nographic \
+    -machine virtualization=true \
+    -kernel out/reference/qemu_aarch64_clang/hafnium.bin \
+    -initrd ./dbg/qemu_aarch64/initrd.img \
+    -append "rdinit=/sbin/init" \
+    -s -S
+
+popd >& /dev/null
--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cd ..
-qemu-system-aarch64 -M virt,gic_version=3 -cpu cortex-a57 -smp 4 -nographic -machine virtualization=true -kernel out/reference/qemu_aarch64_clang/hafnium.bin -initrd ./dbg/initrd.img -append "rdinit=/sbin/init"
-cd - >& /dev/null
+pushd .. >& /dev/null
+
+../qemu-4.1.0/aarch64-softmmu/qemu-system-aarch64 \
+    -M virt,gic_version=3 \
+    -cpu cortex-a57 \
+    -smp 4 \
+    -nographic \
+    -machine virtualization=true \
+    -kernel out/reference/qemu_aarch64_clang/hafnium.bin \
+    -initrd ./dbg/qemu_aarch64/initrd.img \
+    -append "rdinit=/sbin/init"
+
+popd >& /dev/null
